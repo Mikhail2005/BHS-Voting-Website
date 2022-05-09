a9cbb48dc3d4a1e06c0fb78f821e8f9b68f9871f
Mikhail2005 <mikhailkuchciak@gmail.com>
2022-05-10 09:12:07 +1200

    Button Redirects User To Prefects Page

--- website/views.py
+++ website/views.py
@@ -4,3 +4,11 @@ from flask import Blueprint, render_template
 @app.route('/', methods=['GET', 'POST'])
 def divison():
     return render_template("division.html")
+
+@app.route('/prefects', methods=['GET', 'POST'])
+def prefects():
+    return render_template("prefects.html")
+
+#@app.route('/', methods=['GET','POST'])
+#def redirect():
+    #return render_template("prefects.html")
